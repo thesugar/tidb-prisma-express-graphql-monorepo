39e6897998a46c71b62ab5ffc90d18d0eda59ea1
ryoheisato <thesugar87@gmail.com>
2025-02-05 22:58:15 +0900

    ApolloClient に渡す uri は環境変数を用いるように

--- apps/frontend/src/graphql/client.ts
+++ apps/frontend/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5050/graphql', // GraphQL サーバーのエンドポイント
+  uri: import.meta.env.VITE_BACKEND_URL, // GraphQL サーバーのエンドポイント
   cache: new InMemoryCache(),          // キャッシュの設定
 });
 
